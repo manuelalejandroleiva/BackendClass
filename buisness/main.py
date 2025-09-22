c84cb82c08e5ecc0356a5b3ba16d5254d58302b9
Manuel <manuelalejandroleivaferrer@gmail.com>
2025-09-22 19:00:30 -0400

    Deleete a licence

--- buisness/main.py
+++ buisness/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import AsyncSession
 from connection.database import get_db, engine, Base
 from schema import BuisnessCreate,LicenciaCreate
-from service import create_buisness_service,create_licencia_service,get_licencias_service,get_buisnesses_service,get_buisness_by_id_service,update_buisness_service,delete_buisness_service
+from service import create_buisness_service,create_licencia_service,get_licencias_service,get_buisnesses_service,get_buisness_by_id_service,update_buisness_service,delete_buisness_service,delete_licencia_service
 from typing import Dict     
 
 from fastapi import Depends, HTTPException
@@ -90,7 +90,7 @@ async def get_buisness_by_id(buisness_id: int, db: AsyncSession = Depends(get_db
     except Exception as exc:
         raise HTTPException(status_code=500, detail=f"Error inesperado: {str(exc)}")
     
-    
+
 
 @app.delete("/buisness/{buisness_id}", response_model=None)
 async def delete_buisness(buisness_id: int, db: AsyncSession = Depends(get_db)):
@@ -103,3 +103,19 @@ async def delete_buisness(buisness_id: int, db: AsyncSession = Depends(get_db)):
         raise http_exc  # Propaga errores 404 u otros que ya definiste
     except Exception as exc:
         raise HTTPException(status_code=500, detail=f"Error inesperado: {str(exc)}")
+    
+
+
+
+@app.delete("/licence/{licence_id}", response_model=None)
+async def delete_licence(licence_id: int, db: AsyncSession = Depends(get_db)):
+    """ Elimina una licencia por su ID.
+    """
+    try:
+        buisness = await delete_licencia_service(db, licence_id)
+        return buisness
+    except HTTPException as http_exc:
+        raise http_exc  # Propaga errores 404 u otros que ya definiste
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Error inesperado: {str(exc)}")
+    
