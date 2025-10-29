b8d0e7e61082ac6bdaf88dd1f32ccc316ad2c589
Manuel <manuelalejandroleivaferrer@gmail.com>
2025-10-28 21:51:58 -0400

    Fix Create Buisness Functionality

--- buisness/models/models.py
+++ buisness/models/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker, declarative_base
-from database.database import Base
+from buisness.database.database import Base
 
 
 
